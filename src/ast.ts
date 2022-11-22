4ed7fec6037452dcc0920d84635d253c6fe74732
tr00 <tryyyy@mailbox.org>
2022-11-22 19:02:00 +0100

    updated ast

--- src/ast.ts
+++ src/ast.ts
@@ -8,139 +8,93 @@ export enum Kind {
     let = "let",
 }
 
-export type Node =
-    | Module
-    | Symbol
-    | Integer
-    | Lambda
-    | Apply
-    | Block;
-
-export type Module = {
-    kind: Kind.mod;
-    name: string;
-    args: Node[];
-    type?: Type;
-};
-
-export type Symbol = {
-    kind: Kind.sym;
-    name: string;
-    type?: Type;
-};
-
-export type Integer = {
-    kind: Kind.int;
-    value: number;
-    type: Type;
-};
-
-export type Lambda = {
-    kind: Kind.abs;
-    args: Symbol[];
-    body: Node;
-    type?: Type;
-};
-
-export type Apply = {
-    kind: Kind.app;
-    func: Node;
-    args: Node[];
-    type?: Type;
-};
-
-export type Block = {
-    kind: Kind.blk;
-    type?: Type;
-    args: Node[];
-};
-
-export type Assign = {
-    kind: Kind.let;
-    type?: Type;
-    name: string;
-    expr: Node;
-};
-
-export function createModule(
-    { name, args }: { name: string; args: Node[] },
-): Module {
-    return { kind: Kind.mod, name, args };
+export interface Node {
+    nodekind: Kind;
+    children: Node[];
+    original: string;
 }
 
-export function createSymbol({ name }: { name: string }): Symbol {
-    return { kind: Kind.sym, name };
-}
+export class Module implements Node {
+    readonly nodekind = Kind.mod;
+    readonly children: Node[];
+    readonly original = "not yet implemented :[";
 
-export function createInteger({ value }: { value: string | number }): Integer {
-    if (typeof value == "string") value = parseInt(value);
-
-    return { kind: Kind.int, type: KV_INT_T, value };
-}
-
-export function createLambda(
-    { args, body }: { args: Symbol[]; body: Node },
-): Lambda {
-    return { kind: Kind.abs, args, body };
-}
-
-export function createApply(
-    { func, args }: { func: Node; args: Node[] },
-): Apply {
-    return { kind: Kind.app, func, args };
-}
-
-export function createBlock({ args }: { args: Node[] }): Block {
-    return { kind: Kind.blk, args };
-}
-
-export function createAssign(
-    { name, expr }: { name: string; expr: Node },
-): Assign {
-    return { kind: Kind.let, name, expr };
-}
-
-export function writeSyntaxTreeToFile(ast: Node, file = "res/ast.json") {
-    Deno.writeTextFile(file, JSON.stringify(ast));
-}
-
-// type system
-
-abstract class Type {
-    abstract toTypeDef(name: string): string;
-}
-
-export class PrimType extends Type {
-    value: string;
-
-    constructor(value: string) {
-        super();
-
-        this.value = value;
-    }
-
-    toTypeDef(name: string): string {
-        return `${this.value} ${name}`;
+    constructor(children: Node[]) {
+        this.children = children;
     }
 }
 
-export const KV_INT_T = new PrimType("kv_int_t");
-export const KV_ANY_T = new PrimType("kv_any_t");
+export class Integer implements Node {
+    readonly nodekind = Kind.int;
+    readonly children = [];
+    readonly original: string;
 
-export class FuncType extends Type {
-    ptype: Type[];
-    rtype: Type;
-
-    constructor(ptype: Type[], rtype: Type) {
-        super();
-
-        this.ptype = ptype;
-        this.rtype = rtype;
+    constructor(original: string) {
+        this.original = original;
     }
 
-    toTypeDef(name: string): string {
-        const { rtype, ptype } = this;
+    readonly value = () => parseInt(this.original);
+}
 
-        return `${rtype} (*${name})(${ptype.join(", ")})`;
+export class Symbol implements Node {
+    readonly nodekind = Kind.sym;
+    readonly children = [];
+    readonly original: string;
+
+    constructor(original: string) {
+        this.original = original;
     }
+
+    readonly value = () => this.original;
+}
+
+export class Lambda implements Node {
+    readonly nodekind = Kind.abs;
+    readonly children: Node[];
+    readonly original = "welp";
+
+    constructor(children: Node[]) {
+        this.children = children;
+    }
+
+    readonly body = () => this.children[0];
+    readonly args = () => this.children.slice(1) as Symbol[];
+}
+
+export class Apply implements Node {
+    readonly nodekind = Kind.app;
+    readonly children: Node[];
+    readonly original = "(something is missing)";
+
+    constructor(children: Node[]) {
+        this.children = children;
+    }
+
+    readonly func = () => this.children[0];
+    readonly args = () => this.children.slice(1);
+}
+
+export class Block implements Node {
+    readonly nodekind = Kind.blk;
+    readonly children: Node[];
+    readonly original = "{ /* temp */ }";
+
+    constructor(children: Node[]) {
+        this.children = children;
+    }
+
+    // ...
+}
+
+export class Assign implements Node {
+    readonly nodekind = Kind.let;
+    readonly children: Node[];
+    readonly original = "some = thing";
+
+    constructor(children: Node[]) {
+        this.children = children;
+    }
+
+    readonly name = () => this.children[0]; // as Symbol
+    readonly expr = () => this.children[1];
 }
